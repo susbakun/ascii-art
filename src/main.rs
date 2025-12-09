4c950538cf08f7496e6f1b19b41de4c8b4b3cce8
AmirSaeed AryanMehr <amiraryanmehr82@gmail.com>
2025-12-09 21:36:40 +0330

    added inverting and method for calculating the brightness

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,65 @@
 #![allow(clippy::all, clippy::pedantic)]
 
 use image::{ImageReader, imageops::FilterType};
-use std::env;
+use getopts::Options;
 
 const ASCII_CHARS: &'static str = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$";
 
+struct Color {
+    r: u8,
+    g: u8,
+    b: u8
+}
+
+enum BrightnessMethod {
+    Average,
+    MinMax,
+    Luminosity
+}
+
+fn get_brightness_method(method: &str) -> BrightnessMethod {
+    match method {
+        "min_max" => BrightnessMethod::MinMax,
+        "luminosity" => BrightnessMethod::Luminosity,
+        _ => BrightnessMethod::Average,
+    }
+}
+
+fn brightness_map(color: &Color, method: &BrightnessMethod) -> u8 {
+    let Color {r , g, b} = color;
+
+    // converting channels to f32 (preventing overflow)
+    let r = *r as f32;
+    let g = *g as f32;
+    let b = *b as f32;
+
+    match method {
+        BrightnessMethod::Average => ((r + g + b) / 3.0) as u8,
+        BrightnessMethod::MinMax => {
+            let min = [r, g, b].into_iter()
+                .min_by(|a, b| a.partial_cmp(b)
+                .unwrap_or(std::cmp::Ordering::Equal)
+            ).unwrap_or(r);
+
+            let max = [r, g, b].into_iter()
+                .max_by(|a, b| a.partial_cmp(&b)
+                .unwrap_or(std::cmp::Ordering::Equal)
+            ).unwrap_or(r);
+
+            ((min + max) / 2.0) as u8 
+        },
+        BrightnessMethod::Luminosity => (0.21 * r + 0.72 * g + 0.07 * b) as u8
+    }
+}
+
 // Map brightness value (0-255) to ASCII character index (0-63)
-fn linear_map(value: usize) -> usize {
+fn linear_map(value: usize, invert: bool) -> usize {
+    let value = if invert {
+        255 - value
+    }else {
+        value
+    };
+
     (value * 63) / 255
 }
 
@@ -22,11 +75,45 @@ fn print_image(image: &Vec<Vec<char>>) {
     });
 }
 
+fn print_usage(opts: &Options) {
+    let brief = format!("Usage: [options]");
+    println!("{}", opts.usage(&brief));
+}
+
 fn main() {
-    let file_path =  env::args()
-        .skip(1)
-        .next()
-        .expect("You should provide a valid path");
+    let mut opts = getopts::Options::new();
+
+    opts.optopt("m", "method", "Brightness method",
+        "Minmax or average");
+    opts.optopt("f", "file", "Image file name", 
+        "File Name");
+
+    opts.optflag("i", "invert", "Invert color");
+    opts.optflag("h", "help", "Print help menu");
+
+    let matches = opts.parse(std::env::args().skip(1))
+        .unwrap_or_else(|f| {
+            eprintln!("Error: {}", f);
+            print_usage(&opts);
+            std::process::exit(1);
+        });
+    
+    if matches.opt_present("h"){
+        print_usage(&opts);
+        return;
+    }
+
+    let str_arg = |flag: &str, default: &str| -> String {
+        matches.opt_str(flag).unwrap_or(default.to_string())
+    };
+
+
+    let brightness_method_str = str_arg("m", "average");
+    let brightness_method = get_brightness_method(&brightness_method_str);
+
+    let file_path = str_arg("f", "./kaiji_kun.jpg");
+
+    let invert = matches.opt_present("i");
 
     let img = ImageReader::open(file_path)
         .expect("Couldn't read the file");
@@ -56,11 +143,13 @@ fn main() {
     pixel_matrix.iter().for_each(|row| {
         let mut row_pixels = vec![];
         row.iter().for_each(|pixel| {
-            let r = pixel[0] as f32;
-            let g = pixel[1] as f32;
-            let b = pixel[2] as f32;
-            let average = ((r + g + b) / 3.0).floor() as u8;
-            row_pixels.push(average);
+            let r = pixel[0];
+            let g = pixel[1];
+            let b = pixel[2];
+            let color = Color { r, g, b };
+
+            let brightness = brightness_map(&color, &brightness_method);
+            row_pixels.push(brightness);
         });
         brightness_matrix.push(row_pixels);
     });
@@ -69,8 +158,8 @@ fn main() {
     brightness_matrix.iter().for_each(|row| {
         let mut row_asciis = vec![];
         row.iter().for_each(|brightness| {
-            let mapped_value = linear_map(*brightness as usize);
-            let ascii_char = ASCII_CHARS.chars().nth(mapped_value)
+            let mapped_value_index = linear_map(*brightness as usize, invert);
+            let ascii_char = ASCII_CHARS.chars().nth(mapped_value_index)
                 .unwrap_or('a');
             row_asciis.push(ascii_char);
         });
